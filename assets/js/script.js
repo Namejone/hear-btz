6ec7e709074d7ebffa43af4ab57dcb99fdc2974d
namejone <devnamejone@gmail.com>
2025-02-20 13:24:30 +0600

    Sidebar issues fixxed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,21 @@ document
   .addEventListener('click', function () {
     document.querySelector('.root').classList.toggle('show')
   })
+
+const btnBar = document.querySelector('.btn-bar')
+const sidebar = document.querySelector('.sidebar')
+
+// Button click event - Sidebar show/hide
+btnBar.addEventListener('click', (event) => {
+  event.stopPropagation() // Prevent click from reaching document
+  sidebar.classList.toggle('left-0')
+  sidebar.classList.toggle('-left-52')
+})
+
+// Click outside to close sidebar
+document.addEventListener('click', (event) => {
+  if (!sidebar.contains(event.target) && !btnBar.contains(event.target)) {
+    sidebar.classList.add('-left-52')
+    sidebar.classList.remove('left-0')
+  }
+})
